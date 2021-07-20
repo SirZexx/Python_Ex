aa7ec0f5a330d4aa6dc704a98c2a6bba3084ff05
SirZexx <74374828+SirZexx@users.noreply.github.com>
2021-07-20 13:08:13 +0530

    ex1

---
+++ loop/ex1.py
@@ -0,0 +1,15 @@
+# Write a program to make sum of first 50 numbers which are divisible by 11 and also display it on screen
+
+# using While loop
+
+count=0
+sum=0
+num=0
+
+while count<=50:
+    num = num +1
+    if num%11==0:
+        sum=sum+num
+        count = count + 1
+        
+print(f"Sum of first 50 numbers divisible by 11 is {sum}")
