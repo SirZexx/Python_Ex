ffa6d6b4b4955b33267dbfe9b067add158f9c6af
SirZexx <74374828+SirZexx@users.noreply.github.com>
2021-07-21 21:28:05 +0530

    write a program to print & count all values in tuples

---
+++ loop1/ex3.py
@@ -0,0 +1,7 @@
+# 3) write a program to print & count all values in tuples 
+
+day=('sunday','monday','tuesday','wednesday','thursday','friday','saturday')
+count=0
+for count in range(0,len(day)):
+    print(day[count])
+print(len(day))
