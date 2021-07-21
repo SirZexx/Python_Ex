573116e312b6099451c9cbfda5777d152e95e52d
SirZexx <74374828+SirZexx@users.noreply.github.com>
2021-07-21 21:46:35 +0530

    factorial

---
+++ loop/ex3.py
@@ -0,0 +1,15 @@
+# Write a program to findout factorial of given number 
+# input = 5
+# output = 120
+# process = 5 x 4 x 3 x 2 x 1 = 120
+
+num=int(input("Enter the number you want to calculate factorial of : "))
+count=0
+fact=1
+
+while count<=num+1:
+    count=count+1
+    fact=num*fact
+    num=num-1
+    
+print(fact)
