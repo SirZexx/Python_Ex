af27e145b70fdc0f41577ed0f681be59229b9904
SirZexx <74374828+SirZexx@users.noreply.github.com>
2021-07-21 22:04:20 +0530

    factorial

--- loop/ex3for.py
+++ loop/ex3for.py
@@ -2,15 +2,16 @@
 # input = 5
 # output = 120
 # process = 5 x 4 x 3 x 2 x 1 = 120
-# using while loop
+# using for loop
 
 num=int(input("Enter the number you want to calculate factorial of : "))
 count=0
 fact=1
-
-while count<=num+1:
+for count in range (0,num+1):
     count=count+1
-    fact=num*fact
-    num=num-1
-    
+    if num>=1:
+        fact=num*fact
+        num=num-1
+    # print(num)
+
 print(fact)
