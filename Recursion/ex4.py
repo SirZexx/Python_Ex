70e585c85d91db01eef336ff8bcadd1ed57b3cca
SirZexx <74374828+SirZexx@users.noreply.github.com>
2021-08-03 22:39:04 +0530

    push

--- Recursion/ex4.py
+++ Recursion/ex4.py
@@ -1,2 +1,15 @@
 # write a program to print first 50 fibonacci number in reverse
-
+count=1
+previous=1
+current=1
+def fibonacci():
+    if count<=50:
+        next=previous+current
+        previous=current
+        current=next
+        count+=1
+        fibonacci(next)
+        print(next,end=" ")
+        
+        
+fibonacci()
