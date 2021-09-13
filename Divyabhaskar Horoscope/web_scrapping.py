103b4ac6e38492edc57a7afe2d30e0bda91e5c13
SirZexx <74374828+SirZexx@users.noreply.github.com>
2021-09-13 12:33:26 +0530

    final push

--- Divyabhaskar Horoscope/web_scrapping.py
+++ Divyabhaskar Horoscope/web_scrapping.py
@@ -1,30 +1,38 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from datetime import date
+import os
 
-URL = "https://www.divyabhaskar.co.in/rashifal/13/today/"
-r = requests.get(URL)
+today = date.today()
+os.mkdir(f"F:\Work\Python Lessons\Exercise\Divyabhaskar Horoscope\Daily_Rashi\{today}")
 
-soup = bs(r.content,'html.parser')
-file = open("F:\Work\Python Lessons\Exercise\Divyabhaskar Horoscope\Output\Test.txt","w",encoding='utf8')
-text_type = soup.findAll(['h1','p'])
-for text in text_type:
-        file.write(f"{text.text}\n")
-file.close()
+Rashi = {'Aries':13,'Taurus':15,'Gemini':16,'Cancer':19,'Leo':17,'Virgo':18,'Libra':20,'Scorpio':14,'Sagittarius':21,'Capricorn':22,'Aquarius':23,'Pisces':24}
 
-line_num=0
-rmv_line=[1,2,16,17,18,19,20,21,22,23,24,25,26,27]
-data=""
-data1=""
+for name,value in Rashi.items():
+    URL = f"https://www.divyabhaskar.co.in/rashifal/{value}/today/"
+    r = requests.get(URL)
 
-file= open("F:\Work\Python Lessons\Exercise\Divyabhaskar Horoscope\Output\Test.txt",'r+',encoding='utf8')
-for line in file:
-    line_num+=1
-    if line_num in rmv_line:
-        line=[]
-    data=''.join(line)
-    data1=(data1+data)
-file.close()
+    soup = bs(r.content,'html.parser')
+    file = open(f"F:\Work\Python Lessons\Exercise\Divyabhaskar Horoscope\Daily_Rashi\{today}\{name}.txt","w",encoding='utf8')
+    text_type = soup.findAll(['h1','p'])
+    for text in text_type:
+            file.write(f"{text.text}\n")
+    file.close()
 
-file= open("F:\Work\Python Lessons\Exercise\Divyabhaskar Horoscope\Output\Test.txt",'w',encoding='utf8')
-file.write(data1)
-file.close()
+    line_num=0
+    rmv_line=[1,2,16,17,18,19,20,21,22,23,24,25,26,27]
+    data=""
+    data1=""
+
+    file= open(f"F:\Work\Python Lessons\Exercise\Divyabhaskar Horoscope\Daily_Rashi\{today}\{name}.txt",'r+',encoding='utf8')
+    for line in file:
+        line_num+=1
+        if line_num in rmv_line:
+            line=[]
+        data=''.join(line)
+        data1=(data1+data)
+    file.close()
+
+    file= open(f"F:\Work\Python Lessons\Exercise\Divyabhaskar Horoscope\Daily_Rashi\{today}\{name}.txt",'w',encoding='utf8')
+    file.write(data1)
+    file.close()
